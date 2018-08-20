02442df84df3069263a769f89809ce365bda3f73
Priyaranjan1993 <priyaranjan9090@gmail.com>
2018-08-20 04:51:00 -0400

    url fixes

--- js/main.js
+++ js/main.js
@@ -72,11 +72,38 @@ $(window).on('load', function() {
 	/*------------------
 		Popup
 	--------------------*/
-	$('.portfolio-item .port-pic').magnificPopup({
+	/*$('.portfolio-item .port-pic').magnificPopup({
 		type: 'image',
 		mainClass: 'img-popup-warp',
 		removalDelay: 500,
 	});
+*/
+        $('.portfolio-item .port-pic').magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            closeOnContentClick: true,
+            closeBtnInside: true,
+            mainClass: 'img-popup-warp',
+            removalDelay: 500,
+            image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                    return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+                }
+            },
+            gallery: {
+                enabled: true
+            },
+            zoom: {
+                enabled: true,
+                duration: 300, // don't foget to change the duration also in CSS
+                opener: function(element) {
+                    return element.find('img');
+                }
+            }
+
+        });
+
 
 
 
